de05495c598941a462a87e904087174d054a13df
Auwalu Muhammad Izziddin <auwalmuhammad5559@gmail.com>
2025-12-18 10:48:40 -0800

    fixes and booking page

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,25 @@
-import React from 'react'
-import Landing from './pages/Landing'
+// App.jsx
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer'
+import Bookings from './pages/Bookings';
+import Landing from './pages/landing';
 
-const App = () => {
+function App() {
   return (
-    <div>
-      <Landing />
-    </div>
-  )
+    <Router>
+      <Navbar />
+      <main className="pt-16"> 
+        <Routes>
+          <Route path="/" element={<Landing />} />
+      
+          <Route path="/bookings" element={<Bookings />} />
+        </Routes>
+      </main>
+      <Footer />
+    </Router>
+  );
 }
 
-export default App
+export default App;
